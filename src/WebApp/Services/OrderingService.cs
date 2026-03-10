0a0ef594a2c549c0dcfea8413a8f64052c1c0911
Adarsh kumar <adarsh.kumar@harness.io>
2026-03-10 11:30:55 +0530

    Fix: Add api-version parameter to OrderingService API calls

--- src/WebApp/Services/OrderingService.cs
+++ src/WebApp/Services/OrderingService.cs
@@ -1,4 +1,4 @@
-﻿namespace eShop.WebApp.Services;
+namespace eShop.WebApp.Services;
 
 public class OrderingService(HttpClient httpClient)
 {
@@ -6,12 +6,12 @@ public class OrderingService(HttpClient httpClient)
 
     public Task<OrderRecord[]> GetOrders()
     {
-        return httpClient.GetFromJsonAsync<OrderRecord[]>(remoteServiceBaseUrl)!;
+        return httpClient.GetFromJsonAsync<OrderRecord[]>($"{remoteServiceBaseUrl}?api-version=1.0")!;
     }
 
     public Task CreateOrder(CreateOrderRequest request, Guid requestId)
     {
-        var requestMessage = new HttpRequestMessage(HttpMethod.Post, remoteServiceBaseUrl);
+        var requestMessage = new HttpRequestMessage(HttpMethod.Post, $"{remoteServiceBaseUrl}?api-version=1.0");
         requestMessage.Headers.Add("x-requestid", requestId.ToString());
         requestMessage.Content = JsonContent.Create(request);
         return httpClient.SendAsync(requestMessage);
